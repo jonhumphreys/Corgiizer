337eedfc3f06de7bf7db24d09389c53a9b3aeb22
jonhumphreys <93622145+jonhumphreys@users.noreply.github.com>
2021-11-02 14:57:45 -0700

    Initial commit

---
+++ Assets/Scripts/Corgiizer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Corgiizer : MonoBehaviour
+{
+    public Image CorgiImage;
+    public List<Sprite> CorgiSprites;
+    public CanvasGroup CorgiImageCanvasGroup;
+    private bool isShowingImage = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetRandomImage();
+    }
+
+    public void SetRandomImage()
+    {
+        int randomIndex = Random.Range(0, CorgiSprites.Count);
+        CorgiImage.sprite = CorgiSprites[randomIndex];
+    }
+
+    public void ToggleImageVisibility()
+    {
+        if (isShowingImage)
+        {
+            CanvasGroupDisplayer.Hide(CorgiImageCanvasGroup);
+        }
+        else
+            CanvasGroupDisplayer.Show(CorgiImageCanvasGroup);
+        isShowingImage = !isShowingImage;
+    }
+}
